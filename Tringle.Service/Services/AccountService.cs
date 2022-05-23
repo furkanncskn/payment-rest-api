b437fd7b586dd0b852163621cac48e0daf8f6d50
Furkan Coskun <furkancoskunn96@gmail.com>
2022-05-23 14:27:08 +0300

    Add
    Exist ve DeleteById metotları AccountService'e eklendi.
    
    (Generic repository ve servise de ekleme yapilabilirdi fakat amac test etmek oldugu icin bu sekilde yapildi)

--- Tringle.Service/Services/AccountService.cs
+++ Tringle.Service/Services/AccountService.cs
@@ -1,6 +1,7 @@
 ﻿using Tringle.Core.Entities;
 using Tringle.Core.Repositories;
 using Tringle.Core.Services;
+using Tringle.Service.Exceptions;
 
 namespace Tringle.Services.Service
 {
@@ -11,6 +12,17 @@ namespace Tringle.Services.Service
         public AccountService(ITringleRepository<Account> repository, IAccountRepository accountRepository) : base(repository)
         {
             _accountRepository = accountRepository;
+        }
+
+        public Task<bool> ExistAsync(Account account)
+        {
+            return _accountRepository.AnyAsync(p => p.AccountNumber == account.AccountNumber);
+        }
+
+        public Task DeleteByIdAsync(int id)
+        {
+            var account = _accountRepository.WhereAsync(p => p.AccountNumber == id).Result.SingleOrDefault() ?? throw new ClientSideException($"{id} not exist.");
+            return _accountRepository.DeleteAsync(account);
         }
     }
 }
